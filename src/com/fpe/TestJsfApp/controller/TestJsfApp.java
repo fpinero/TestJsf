b3a464b16ccf6360fd911ee115c9f0ed263715c5
Fernando Piñero <grpeto@gmail.com>
2017-06-12 23:05:24 +0200

    added ManagedBean

--- src/com/fpe/TestJsfApp/controller/TestJsfApp.java
+++ src/com/fpe/TestJsfApp/controller/TestJsfApp.java
@@ -1,17 +1,34 @@
 package com.fpe.TestJsfApp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.fpe.TestJsf.Domain.Student;
 
 @Controller
+@EnableWebMvc
 public class TestJsfApp {
+	
+	@Autowired
+	Student student;
 	
 	@RequestMapping("/")
 	public String doThings(Model model){
-		
+				
 		return("hello-world");
 		
 	}
+	
+	@RequestMapping("/form")
+	public String doThings2(Model model){
+				
+		return("student-form");
+		
+	}
+	
+	
 
 }
